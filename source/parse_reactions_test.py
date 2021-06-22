53f70c637b260fdb2d94e3929fdd560d6f15f94a
lucas.breitsameter <lucas.breitsameter@indiaca.de>
2021-06-22 12:05:36 +0200

    Fixed test case

--- source/parse_reactions_test.py
+++ source/parse_reactions_test.py
@@ -19,7 +19,7 @@ class TestParseReactions(unittest.TestCase):
 
     def test_as_reaction_and_performer(self):
         parsed_reaction = ReactionParser.parse_comment("(Beifall bei der CDU/CSU, der SPD, der FDP, der LINKEN und dem BÜNDNIS 90/DIE GRÜNEN – Zuruf der Abg. Beatrix von Storch [AfD])")
-        self.assertEqual(parsed_reaction, "BEIFALL_CDU_CSU_FDP_GRUENE_LINKE_SPD+ZURUF_AFD")
+        self.assertEqual(parsed_reaction, "BEIFALL_CDU_CSU_FDP_GRUENE_LINKE_SPD ZURUF_AFD")
 
 
 if __name__ == '__main__':
