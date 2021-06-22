6be906e75f81212b1e211be0489ff15c5988f85d
wang yashu <wangyashuu@gmail.com>
2021-06-22 10:42:38 +0200

    fix: parse a comment string and return category string

--- source/parse/parse_reactions.py
+++ source/parse/parse_reactions.py
@@ -9,8 +9,8 @@ def tokenize(comments):
     nltk.download('punkt', download_dir='downloads_cache')
     tokens = [token.lower() for c in comments for token in nltk.word_tokenize(c)]
     return tokens
-    
-    
+
+
 def filter_words(tokens):
 
     # remove single punctuation: "[", "]", ":", "," etc
@@ -19,7 +19,7 @@ def filter_words(tokens):
     # remove stopwords
     nltk.download('stopwords', download_dir='downloads_cache')
     tokens = [t for t in tokens if t not in stopwords.words('german')]
-    
+
     return tokens
 
 
@@ -29,6 +29,7 @@ def analyze_words(tokens, top_count=50):
     selected_words = {k: v for k, v in itertools.islice(sorted_fdict.items(), 0, top_count)}
 
     return selected_words
+
 
 reactions = [['beifall'], ['zuruf', 'zurufe'], ['lachen'], ['heiterkeit'], ['sagen', 'sagt'], ['reden', 'rede'], ['hört'], ['wissen'], [ 'steht'], [ 'lesen'], ['kommen', 'kommt'], ['erzählen']]
 performers =  [['spd'], ['cdu/csu'], ['90/die grünen', '90/die' , '90/diegrünen'] ,['fdp'], ['afd'], ['linken', 'linke']]
@@ -41,4 +42,13 @@ def as_reaction_and_performer(comments, reactions=reactions, performers=performe
             "reactions": [rarr[0] for rarr in reactions if any(r in c for r in rarr)],
             "performers": [parr[0] for parr in performers if any(p in c for p in parr)]
         })
+    return result
+
+
+def parse_comment(comment):
+    c = comment.lower()
+    result = str({
+        "reactions": [rarr[0] for rarr in reactions if any(r in c for r in rarr)],
+        "performers": [parr[0] for parr in performers if any(p in c for p in parr)]
+    })
     return result
