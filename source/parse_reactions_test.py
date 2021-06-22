b5e21064d01e7536dc7a666463f4ea9d55f8c466
lucas.breitsameter <lucas.breitsameter@indiaca.de>
2021-06-22 13:20:52 +0200

    Added further tests

--- source/parse_reactions_test.py
+++ source/parse_reactions_test.py
@@ -6,6 +6,21 @@ class TestParseReactions(unittest.TestCase):
         parsed_reaction = ReactionParser.parse_comment("(Beifall bei der CDU/CSU, der SPD, der FDP, der LINKEN und dem BÜNDNIS 90/DIE GRÜNEN – Zuruf der Abg. Beatrix von Storch [AfD])")
         self.assertEqual(parsed_reaction, "BEIFALL_CDU_CSU_FDP_GRUENE_LINKE_SPD ZURUF_AFD")
 
+    def test_as_reaction_and_performer_2(self):
+        parsed_reaction = ReactionParser.parse_comment("(Beatrix von Storch [AfD]: Ja, ja!)")
+        self.assertEqual(parsed_reaction, "SPEECH_AFD")
+
+    def test_as_reaction_and_performer_3(self):
+        parsed_reaction = ReactionParser.parse_comment("Beifall der Abg. Ulli Nissen [SPD]")
+        self.assertEqual(parsed_reaction, "BEIFALL_SPD")
+
+    def test_as_reaction_and_performer_4(self):
+        parsed_reaction = ReactionParser.parse_comment("(Lachen beim BÜNDNIS 90/DIE GRÜNEN)")
+        self.assertEqual(parsed_reaction, "LACHEN_GRUENE")
+
+    def test_as_reaction_and_performer_5(self):
+        parsed_reaction = ReactionParser.parse_comment("Die Abgeordneten der  Fraktion  der  LINKEN  halten  Spruchbänder hoch")
+        self.assertEqual(parsed_reaction, "UNSPECIFIED_LINKE")
 
 if __name__ == '__main__':
     unittest.main()
